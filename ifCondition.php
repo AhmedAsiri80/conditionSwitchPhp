8416635e600e4a99f977b0d7fa025a96c3495825
ahmed <a707a7med@gmail.com>
2022-04-02 06:26:12 +0300

    switch and if condition

---
+++ ifCondition.php
@@ -0,0 +1,26 @@
+<?php
+// الجمل الشرطية
+
+$x = 30;
+$y = 25;
+/*
+if(condition : $x = $y)
+this is square
+
+else
+this is rectagle
+*/
+
+if ( $x == $y)
+{
+    echo " this is square" ;
+} 
+else if ($x > $y){
+    echo " this is rectangle";
+} else if ($x < $y ) {
+    echo " this is AA"
+}
+
+// vertical / horisontal
+
+?>
---
+++ switch.php
@@ -0,0 +1,20 @@
+<?php
+$month = 3 ; 
+switch($month) {
+case "1":
+echo " Jan";
+break; //  اذا تحقق الشرط اخرج. تكسر سلسلة الشروط وتطلع اذا تحقق الشرط 
+case "2" :
+echo "Feb ";
+break;  // اذا تحقق الشرط اخرج. 
+default:
+echo " no month with this value";
+break;  // اذا تحقق الشرط اخرج. تكسر سلسلة الشروط وتطلع اذا تحقق الشرط 
+}
+
+
+
+
+
+
+?>
